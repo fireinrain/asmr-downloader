c88bf9f14d1ea94bdff6b7089ebf1de9cd3861d4
sunrise <575563079@qq.com>
2023-02-07 01:38:14 +0800

    完成首页采集同步

--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"asmr-downloader/config"
+	"asmr-downloader/model"
 	"asmr-downloader/spider"
 	"asmr-downloader/storage"
 	"asmr-downloader/utils"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
-//func init() {
-//	fmt.Println("------ASMR.ONE Downloader------")
-//	fmt.Println("---------Power by Euler--------")
-//}
+//	func init() {
+//		fmt.Println("------ASMR.ONE Downloader------")
+//		fmt.Println("---------Power by Euler--------")
+//	}
+var pageDataChannel = make(chan model.PageResult, 5)
 
 func main() {
 	println("------ASMR.ONE Downloader------")
@@ -32,8 +35,9 @@ func main() {
 		return
 	}
 	fmt.Println("账号登录成功!")
+	var authStr = asmrClient.Authorization
 	//获取首页
-	indexPageInfo, err := spider.GetIndexPageInfo(asmrClient.Authorization)
+	indexPageInfo, err := spider.GetIndexPageInfo(authStr)
 	if err != nil {
 		log.Println("ASMR one 首页获取失败: ", err.Error())
 	}
@@ -41,20 +45,62 @@ func main() {
 	var totalCount = indexPageInfo.Pagination.TotalCount
 	var pageSize = indexPageInfo.Pagination.PageSize
 	maxPage := utils.CalculateMaxPage(totalCount, pageSize)
+	var subTitleFlag = 0
+	maxPage = 10
 	pool := asmrClient.WorkerPool
-	for i := 2; i < maxPage; i++ { //开启20个请求
-		ii := i
-		pool.Do(func() error {
-			for j := 0; j < 10; j++ { //每次打印0-10的值
-				fmt.Println(fmt.Sprintf("%v->\t%v", ii, j))
-				time.Sleep(1 * time.Second)
-			}
-			//time.Sleep(1 * time.Second)
-			return nil
-		})
-	}
-	pool.Wait()
+	//接受数据
+	//并发10
+	//limiter := make(chan bool, 20)
+	fetchWg := &sync.WaitGroup{}
+	go func() {
+		fetchWg.Add(1)
+		defer fetchWg.Done()
+		defer close(pageDataChannel)
+		for i := 1; i <= maxPage; i++ { //开启20个请求
+			pageIndex := i
+			pool.Do(func() error {
+				return PageDataTaskHandler(authStr, pageIndex, subTitleFlag)
+			})
+		}
+		_ = pool.Wait()
+	}()
+	fetchWg.Wait()
 
+	processWg := &sync.WaitGroup{}
+	go ProcessCollectPageData(processWg)
+	processWg.Wait()
+
+	time.Sleep(10 * time.Second)
+
+}
+
+func PageDataTaskHandler(authStr string, pageIndex int, subTitleFlag int) error {
+	infoData, err2 := spider.GetPerPageInfo(authStr, pageIndex, subTitleFlag)
+	if err2 != nil {
+		fmt.Printf("当前页: %d,访问失败", pageIndex)
+		//TODO 记录失败的index
+	}
+	fmt.Printf("获取到数据页: %d", pageIndex)
+	//发送给channel
+	pageDataChannel <- *infoData
+	//fmt.Printf("数据: %v\n", infoData)
+	return nil
+}
+
+func ProcessCollectPageData(wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+	fmt.Println("process data...")
+	for rc := range pageDataChannel {
+		fmt.Printf("data: %v", rc)
+	}
+	fmt.Println("采集结束")
+	//index := 0
+	//for rc := range pageDataChannel {
+	//	index += 1
+	//	fmt.Printf("序号: %d\n\n", index)
+	//	fmt.Printf("data: %v\n", rc)
+	//}
 }
 
 // CheckIfFirstStart
